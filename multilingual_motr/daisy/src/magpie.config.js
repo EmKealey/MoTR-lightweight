a0580c9eeabe386820bfc31c7b4cc0cdc2c02222
Emma Kealey <kealey@usc.edu>
2026-07-06 20:45:54 -0700

    update server info

--- multilingual_motr/daisy/src/magpie.config.js
+++ multilingual_motr/daisy/src/magpie.config.js
@@ -1,13 +1,11 @@
 export default {
-  experimentId: '1',
-  serverUrl: 'https://cui-motr-new.herokuapp.com/',
-  // serverUrl: 'https://gentle-everglades-00112.herokuapp.com/',
-  socketUrl: 'wss://cui-motr-new.herokuapp.com/socket',
-  // socketUrl: 'wss://gentle-everglades-00112.herokuapp.com/socket',
+  experimentId: '2',
+  serverUrl: 'https://testappegk.gigalixirapp.com',
+socketUrl: 'wss://testappegk.gigalixirapp.com/socket',
   // this will be used in prolific mode
-  completionUrl: 'https://app.prolific.co/submissions/complete?cc=C1I4D2OB',
-  contactEmail: 'cui.ding@uzh.ch',
+  completionUrl: 'https://usc.qualtrics.com/jfe/form/SV_6fjhmLL8c3oLNOu',
+  contactEmail: 'kealey@usc.edu',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'debug',
+  mode: 'prolific',
   language: 'en'
 };
