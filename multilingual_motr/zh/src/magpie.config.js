87066130c942e444c9732f3473fe641310e2adf8
Emma Kealey <emmakealey4@gmail.com>
2026-07-07 20:27:10 -0700

    Update magpie.config.js

--- multilingual_motr/zh/src/magpie.config.js
+++ multilingual_motr/zh/src/magpie.config.js
@@ -3,7 +3,9 @@ export default {
 serverUrl: 'https://testappegk.gigalixirapp.com',
 socketUrl: 'wss://testappegk.gigalixirapp.com/socket',
   // this will be used in prolific mode
-  completionUrl: 'https://github.com/EmKealey/nor/labels',
+  url = new URL(https:emkealey.github.io/MoTR-lightweight/multilingual_motr/daisy/);
+  url.searchParams.set("SubjectId", SubjectId);
+  completionUrl: url.toString();,
   contactEmail: 'kealey@usc.edu',
   uploadUrl: 'https://drive.switch.ch/index.php/s/wlfkWdmKr6eoqSH',
   // Either 'debug', 'directLink' or 'prolific'
