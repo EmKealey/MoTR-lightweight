eb1872491a542b1deb18bff2a5c5443db5c5bdfb
Emma Kealey <emmakealey4@gmail.com>
2026-07-07 19:15:54 -0700

    Update magpie.config.js

--- multilingual_motr/daisy/src/magpie.config.js
+++ multilingual_motr/daisy/src/magpie.config.js
@@ -6,6 +6,6 @@ socketUrl: 'wss://testappegk.gigalixirapp.com/socket',
   completionUrl: 'https://usc.qualtrics.com/jfe/form/SV_6fjhmLL8c3oLNOu',
   contactEmail: 'kealey@usc.edu',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'prolific',
+  mode: 'directLink',
   language: 'en'
 };
